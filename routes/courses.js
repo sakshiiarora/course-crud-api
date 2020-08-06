cddca0670947b307dc2f6d56cab0d1e110833405
vashudevdhama <dhamavashu1119@gmail.com>
2020-08-06 23:31:52 +0530

    feat: POST requests

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fs = require("fs");
+const { parse } = require("path");
 
 const router = express.Router();
-
-// Get APIs
+//====================== Get APIs ======================//
 router.get("/", (req, res) => {
   let coursesData = JSON.parse(fs.readFileSync("json_data/courses.json"));
   res.json({ data: coursesData.data, error: null });
@@ -20,6 +20,69 @@ router.get("/:id", (req, res) => {
     });
   }
   res.json({ data: course, error: null });
+});
+
+//====================== POST APIs ======================//
+// Add course
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const availableSlots = req.body.availableSlots;
+  if (!name || !availableSlots || !description) {
+    return res.status(400).json({ error: "Invalid input data." });
+  }
+  fs.readFile("json_data/courses.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Can't read data" });
+    }
+    obj = JSON.parse(data);
+    obj.data.push({
+      id: obj.data.length > 0 ? obj.data[obj.data.length - 1].id + 1 : 1,
+      name: name,
+      description: description,
+      enrolledStudents: [],
+      availableSlots: availableSlots,
+    });
+    jsonData = JSON.stringify(obj, null, 2);
+    fs.writeFile("json_data/courses.json", jsonData, "utf8", () => {
+      res.json({ success: true });
+    });
+  });
+});
+
+// Enroll student in course
+router.post("/:id/enroll", (req, res) => {
+  const courseId = req.params.id;
+  const studentId = req.body.studentId;
+  let courses = JSON.parse(fs.readFileSync("json_data/courses.json", "utf8"));
+  let students = JSON.parse(fs.readFileSync("json_data/students.json", "utf8"));
+  const course = courses.data.find((course) => {
+    return course.id === parseInt(courseId);
+  });
+  const student = students.data.find((student) => {
+    return student.id === parseInt(studentId);
+  });
+  if (!course) {
+    return res
+      .status(400)
+      .json({ error: `No such course exist with id ${courseId}` });
+  }
+  if (!student) {
+    return res
+      .status(400)
+      .json({ error: `No such student exist with id ${studentId}` });
+  }
+  if (course.availableSlots < 1) {
+    return res.json({ success: false, msg: "No empty slot found." });
+  }
+  courses.data[courseId - 1].enrolledStudents.push({
+    id: student.id,
+    name: student.name,
+  });
+  jsonData = JSON.stringify(courses, null, 2);
+  fs.writeFile("json_data/courses.json", jsonData, "utf8", () => {
+    res.json({ success: true });
+  });
 });
 
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -2,10 +2,32 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 
-// Get APIs
+//====================== Get APIs ======================//
 router.get("/", (req, res) => {
   let studentsData = JSON.parse(fs.readFileSync("json_data/students.json"));
   res.json({ data: studentsData.data, error: null });
+});
+
+//====================== POST APIs ======================//
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  if (!name) {
+    res.status(400).json({ error: "Invalid input." });
+  }
+  fs.readFile("json_data/students.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Couldn't read file." });
+    }
+    let obj = JSON.parse(data);
+    obj.data.push({
+      id: obj.data.length > 0 ? obj.data[obj.data.length - 1].id + 1 : 1,
+      name: name,
+    });
+    let jsonData = JSON.stringify(obj, null, 2);
+    fs.writeFile("json_data/students.json", jsonData, "utf8", () => {
+      return res.json({ success: true });
+    });
+  });
 });
 
 module.exports = router;
